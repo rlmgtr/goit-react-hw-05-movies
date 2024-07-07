3c38157ef29e746218d9d2e64255d7c64870c5f1
rlmgtr <rlmagtira@gmail.com>
2024-07-08 00:38:58 +0800

    update

---
+++ src/pages/Movie/MovieDetailsPage.jsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { fetchMovieDetails } from 'api/api';
+import { useLocation, Outlet } from 'react-router-dom';
+import { Suspense } from 'react';
+import css from './MovieDetailsPage.module.css';
+
+const MovieDetailsPage = () => {
+  const { movieId } = useParams();
+  const [movieDetails, setMovieDetails] = useState(null);
+  const location = useLocation();
+  const backLink = location.state?.from ?? '/movies';
+
+  useEffect(() => {
+    const fetchDetails = async () => {
+      try {
+        const movie = await fetchMovieDetails(movieId);
+        setMovieDetails(movie);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchDetails();
+  }, [movieId]);
+
+  if (!movieDetails) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+      <Link to={backLink} className={css.goBackLink}>
+        <button className={css.goBackButton}>⬅ Go back</button>
+      </Link>
+      <div className={css.movieDetailsContainer}>
+        <img
+          className={css.image}
+          src={
+            movieDetails.poster_path
+              ? `https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`
+              : `https://fakeimg.pl/600x400?text=No+Image+Available`
+          }
+          alt={movieDetails.title}
+        />
+        <div className={css.movieDetailsWrap}>
+          <h1>{movieDetails.title}</h1>
+          <h4>User score: {Math.round(movieDetails.vote_average * 10)}%</h4>
+          <h2>Overview</h2>
+          <p>{movieDetails.overview}</p>
+          <h2>Genres</h2>
+          <p>
+            {movieDetails.genres.map(genre => (
+              <span key={genre.id}> {genre.name}</span>
+            ))}
+          </p>
+        </div>
+      </div>
+
+      <hr />
+      <h3>Additional information</h3>
+      <div className={css.movieNav} >
+      <Link to="cast" className={css.link}>
+        <button className={css.infoButton}>Cast</button>
+      </Link>
+      <Link to="reviews" className={css.link}>
+        <button className={css.infoButton}>Reviews</button>
+      </Link>
+      </div>
+      <hr />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Outlet />
+      </Suspense>
+    </>
+  );
+};
+
+export default MovieDetailsPage;
